688201e100268fd24203038f8b1a566d37fdbbb7
kayceemartin <95602124+kayceemartin@users.noreply.github.com>
2022-01-03 15:35:58 -0500

    updated js css and html

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("BlackJack")
+
+
+let suits = ["spades", "hearts", "clubs", "diamonds"]
+let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+let deck = new Array ();
+let players = new Array ();
+let currentPlayer = 0;
+
+function createDeck() {
+    deck = new Array ();
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < suits.length; j++) {
+            let 
+        }
+    }
+}
