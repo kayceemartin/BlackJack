673dfacf3a0ac47ede82aa738e80dd0a5eb05cc4
kayceemartin <95602124+kayceemartin@users.noreply.github.com>
2022-01-04 16:26:14 -0500

    added a lot of CSS and JS

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 console.log("BlackJack")
 
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-let suits = ["spades", "hearts", "clubs", "diamonds"]
-let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-let deck = new Array ();
-let players = new Array ();
-let currentPlayer = 0;
+class Deck {
+    constructor(cards = newDeck()) {
+        this.cards = cards
+    }
 
-function createDeck() {
-    deck = new Array ();
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < suits.length; j++) {
-            let 
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for(let i = this.numberOfCards - 1; i > 0; i --) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
         }
     }
 }
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function newDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        } )
+    })
+}
+
+const deck = new Deck ()
+deck.shuffle()
+console.log(deck.cards)
